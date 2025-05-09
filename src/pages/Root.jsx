d7bdfd71ba84dc99bd484ab4b650b400c36f47c9
mahnaf <mahnaf125@gmail.com>
2025-05-10 05:58:29 +0600

    Initialization
    Initialized Project | Configured Tailwind CSS | Configured React Router | Completed Analysis

---
+++ src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Root = () => {
+	return (
+		<Fragment>
+			<h1 className="text-5xl font-bold text-center py-4">
+				<span className="bg-light text-primary-light px-2">WEBSITE</span>
+				<span className="bg-light text-dark px-2">IS</span>
+				<span className="bg-dark text-light px-2">UNDER</span>
+				<span className="bg-dark text-primary-dark px-2">DEVELOPMENT</span>
+			</h1>
+		</Fragment>
+	);
+};
+
+export default Root;
