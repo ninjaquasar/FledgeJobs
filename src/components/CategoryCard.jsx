91849b5a3df78f333dea6021d57ebb30449ce24f
mahnaf <mahnaf125@gmail.com>
2025-05-17 04:16:09 +0600

    Dynamic Categories section
    Used dynamic content in Categories section and changed it's Design a bit

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,38 +1,21 @@
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 
-const CategoryCard = ({ category, roundedIconBox = false, setRoundedIconBox }) => {
+const CategoryCard = ({ category }) => {
 	return (
 		<div
 			key={category.name}
-			className={`p-8 bg-slate-200 border border-slate-300 ${category.cardClasses} rounded-2xl transition-colors flex flex-col items-center gap-y-5`}
+			className="p-8 bg-slate-200 border border-slate-300 rounded-2xl flex flex-col items-center gap-y-5 hover:bg-primary/15 transition-colors hover:border-primary/25"
 		>
-			{/* Tooltip */}
-			<Tooltip
-				anchorSelect=".switch-shape"
-				offset={16}
-				style={{
-					paddingInline: "0.5rem",
-					paddingBlock: "0.4rem",
-				}}
-			>
-				Click to change Shape
-			</Tooltip>
 			{/* Category icon box */}
-			<div
-				className={`w-fit ${category.iconClasses} p-4 ${
-					roundedIconBox ? "rounded-full" : "rounded-lg"
-				} cursor-pointer`}
-				onClick={() => setRoundedIconBox(!roundedIconBox)}
-			>
-				<a className="switch-shape">{category.icon}</a>
+			<div className="text-4xl font-semibold text-light bg-primary p-4 aspect-square rounded-xl flex justify-center cursor-default">
+				<a>{category.name[0]}</a>
 			</div>
 			{/* Category content */}
-			<div className="text-center space-y-2">
+			<div className="text-center space-y-3">
 				<h4 className="text-2xl font-bold">{category.name}</h4>
 				<p className="text-slate-600 font-medium">{category.description}</p>
 			</div>
-			<p className="text-slate-500">({category.opportunities} Available Opportunities)</p>
 		</div>
 	);
 };
